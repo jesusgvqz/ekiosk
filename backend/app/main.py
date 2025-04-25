d3b0fc420ff9d22aa1fe936db41e94eaaf3b3ee5
jesusgvqz <jesusgvqz@outlook.es>
2025-04-25 09:20:43 -0600

    added middleware to allow IPs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ekiosk backend")
 
@@ -8,3 +9,12 @@ app.include_router(router)
 @app.get("/")
 def read_root():
     return {"message": "ekiosk backend running 🚀"}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # O ["http://localhost:53031"] si quieres especificar
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
